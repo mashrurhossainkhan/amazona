4ec6802c9edc385e4be957507694972a93862ab2
Mashrur Hossain Khan <khanmashrur03@gmail.com>
2024-03-26 17:01:03 +0600

    forget password CSS

--- src/screens/VerifyForgetPassword.js
+++ src/screens/VerifyForgetPassword.js
@@ -2,56 +2,58 @@ import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 
 const VerifyForgetPassword = (props) => {
-    const [verificationNo, setverificationNo] = useState('');
-    const userForgerPasswordVerficationCode = useSelector((state) => state.verificationCodeForgetPass);
-    const { loading:verficaitonCodeLoading, error:verficaitonCodeError, theVerificationCode} = userForgerPasswordVerficationCode;
-    const history = useHistory();
+  const [verificationNo, setverificationNo] = useState('');
+  const userForgerPasswordVerficationCode = useSelector(
+    (state) => state.verificationCodeForgetPass
+  );
+  const {
+    loading: verficaitonCodeLoading,
+    error: verficaitonCodeError,
+    theVerificationCode,
+  } = userForgerPasswordVerficationCode;
+  const history = useHistory();
 
-   
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-     
-        if(theVerificationCode.theVerificationCode == verificationNo){
-            history.push('/newpassword/'+props.match.params.phnNo);
-        }
-      };
+    if (theVerificationCode.theVerificationCode == verificationNo) {
+      history.push('/newpassword/' + props.match.params.phnNo);
+    }
+  };
 
-    return(
-        <div className='center-div'>
-           {verficaitonCodeLoading?
-            <LoadingBox>Loading...</LoadingBox>:
-            verficaitonCodeError? 
-            <>
-            <MessageBox>No user found</MessageBox>
-            </>:
-            <>
-               
-            </>
-                }
-            <form className="form center-form" onSubmit={submitHandler}>
-                <div>
-                    <input
-                        type="text"
-                        id="phnNo"
-                        className='dialoageInput'
-                        placeholder="Your Verification No"
-                        required
-                        onChange={(e) => setverificationNo(e.target.value)}
-                        ></input>
-                    </div>
-
-                    <div> 
-                        <button className="btn_forgetpass" type="submit">
-                            Reset my password
-                        </button>
-                    </div>
-            </form>
-          
+  return (
+    <div className="center-div">
+      {verficaitonCodeLoading ? (
+        <LoadingBox>Loading...</LoadingBox>
+      ) : verficaitonCodeError ? (
+        <>
+          <MessageBox>No user found</MessageBox>
+        </>
+      ) : (
+        <></>
+      )}
+      <form className="form center-form" onSubmit={submitHandler}>
+        <div>
+          <input
+            type="text"
+            id="phnNo"
+            className="dialoageInput"
+            placeholder="Your Verification No"
+            required
+            onChange={(e) => setverificationNo(e.target.value)}
+          ></input>
         </div>
-    )
-}
+
+        <div>
+          <button className="headerBtn" type="submit">
+            Reset my password
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 export default VerifyForgetPassword;
